0838b19654f7a838a90c297840421af4b6ffc103
seong su kim <know.warehouse02@gmail.com>
2025-05-30 09:25:52 +0900

    //interface

--- src/app/services/question.ts
+++ src/app/services/question.ts
@@ -10,6 +10,7 @@ export interface Question {
   commentCount: number;
 }
 
+// Question 생성 요청 인터페이스
 export const getQuestions = async (): Promise<Question[]> => {
   const response = await axios.get('/questions');
   return response.data;
